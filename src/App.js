17c78f7f0c21caae7559037c5949516514542981
StrangeBee <oduolabenfico@gmail.com>
2022-10-29 09:16:35 +0100

    made adjustments to id

--- src/App.js
+++ src/App.js
@@ -5,42 +5,42 @@ import zuri from './icons/zuri-internship-img.svg';
 import ing from './icons/Ing.png';
 import me from './img/profile.jpg';
 import share from './icons/share-solid.svg';
-import Mshare from './icons/m-share-btn.svg'
+import Mshare from './icons/m-share-btn.svg';
 
 function App() {
   return (
     <div className="container">
       <img src={me} className='p-img' id='profile__img' alt=''/>
-      <p className='p-name' id='twitter' handle='@oduolabenfico'> Oduola Benfico</p>
-      {/*<p className='p-name' id='slack' email='oduolatemmy@gmail.com'> Beneze </p>*/}
+      <p className='p-name' id='twitter'> Oduola Benfico</p>
+      <p className='s-name' id='slack'> Beneze </p>
 
       <img src={share} className='share-btn' />
       <img src={Mshare} className='m-share-btn' />
 
       <div className='section'>
-        <a href="https://www.twitter.com/oduolabenfico">
-          <button className='twitter_link'>Twitter Link</button>
+        <a href="https://www.twitter.com/oduolabenfico" className='twitter_link'>
+          Twitter Link
         </a>
-        <a href="https://training.zuri.team/">
-          <button id='btn__zuri'>Zuri Team</button>
+        <a href="https://training.zuri.team/" id='btn__zuri'>
+          Zuri Team
         </a>
-        <a href="http://books.zuri.team" >
-          <button id='books'>Zuri Books</button>
+        <a href="http://books.zuri.team" id='books' >
+          Zuri Books
         </a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>">
-          <button id='book_python'>Python Books</button>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=<Beneze>" id='book__python' >
+          Python Books
         </a>
-        <a href="https://background.zuri.team">
-          <button id='pitch'>Background Check for Coders</button>
+        <a href="https://background.zuri.team" id='pitch'>
+          Background Check for Coders
         </a>
-        <a href="https://books.zuri.team/design-rules">
-          <button id='book__design'>Design Books</button>
+        <a href="https://books.zuri.team/design-rules" id='book__design'>
+          Design Books
         </a>
-        <div className='icons'>
-          <a href=''><img src={slack} className='slck-icon' alt=''/></a>
-          <a href=''><img src={github} className='github-icon'  alt=''/></a>
-        </div>
      </div>
+     <div className='icons'>
+          <img src={slack} className='slck-icon' alt=''/>
+          <img src={github} className='github-icon'  alt=''/>
+      </div>
 
       <div className='footer'>
         <img src={zuri} className="zuri-icon"  alt=''/>
